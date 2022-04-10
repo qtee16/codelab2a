11beb6a2c0341eb8544cf0fc60c87b92f5155944
qtee16 <thang.tkq16@gmail.com>
2022-04-10 09:28:47 +0700

    final

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount;
     TextView mShowCount;
+    Button btnZero;
+    Button btnCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
         mCount = 0;
         mShowCount = findViewById(R.id.show_count);
+        btnZero = findViewById(R.id.button_zero);
+        btnCount = findViewById(R.id.button_count);
+
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setBackgroundColor(Color.GRAY);
+                mCount = 0;
+                mShowCount.setText("0");
+            }
+        });
+
+
     }
 
     public void showToast(View view) {
@@ -30,5 +47,12 @@ public class MainActivity extends AppCompatActivity {
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
+        if (mCount % 2 == 0) {
+            view.setBackgroundColor(Color.RED);
+        } else {
+            view.setBackgroundColor(Color.GREEN);
+        }
+
+        btnZero.setBackgroundColor(Color.CYAN);
     }
 }
